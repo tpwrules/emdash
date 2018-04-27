e02e1898fd69abe43bfd28912940788277f7adaf
Thomas Watson <twatson52@icloud.com>
2018-04-27 08:15:34 +0200

    initialize gpio without calling into board lib

--- dash_11c24/dsrc/dash_11c24.c
+++ dash_11c24/dsrc/dash_11c24.c
@@ -34,17 +34,13 @@ int main(void) {
 #if defined (__USE_LPCOPEN)
     // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
 #endif
 
 
     __disable_irq();
+
+    // initialize GPIO
+    Chip_GPIO_Init(LPC_GPIO);
 
     // configure SysTick timer to fire every 10ms
     SysTick_Config(SystemCoreClock/100);
--- dash_11c24/dsrc/sysinit.c
+++ dash_11c24/dsrc/sysinit.c
@@ -43,14 +43,55 @@
  * Public types/enumerations/variables
  ****************************************************************************/
 
-#if defined(NO_BOARD_LIB)
 const uint32_t OscRateIn = 12000000;
 const uint32_t ExtRateIn = 0;
-#endif
 
 /*****************************************************************************
  * Private functions
  ****************************************************************************/
+
+/* Setup system clocking */
+// copied from board_sysinit.c
+STATIC void SystemSetupClocking(void)
+{
+	volatile int i;
+
+	/* Powerup main oscillator */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_SYSOSC_PD);
+
+	/* Wait 200us for OSC to be stablized, no status
+	   indication, dummy wait. */
+	for (i = 0; i < 0x100; i++) {}
+
+	/* Set system PLL input to main oscillator */
+	Chip_Clock_SetSystemPLLSource(SYSCTL_PLLCLKSRC_MAINOSC);
+
+	/* Power down PLL to change the PLL divider ratio */
+	Chip_SYSCTL_PowerDown(SYSCTL_POWERDOWN_SYSPLL_PD);
+
+	/* Setup PLL for main oscillator rate (FCLKIN = 12MHz) * 4 = 48MHz
+	   MSEL = 3 (this is pre-decremented), PSEL = 1 (for P = 2)
+	   FCLKOUT = FCLKIN * (MSEL + 1) = 12MHz * 4 = 48MHz
+	   FCCO = FCLKOUT * 2 * P = 48MHz * 2 * 2 = 192MHz (within FCCO range) */
+	Chip_Clock_SetupSystemPLL(3, 1);
+
+	/* Powerup system PLL */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_SYSPLL_PD);
+
+	/* Wait for PLL to lock */
+	while (!Chip_Clock_IsSystemPLLLocked()) {}
+
+	/* Set system clock divider to 1 */
+	Chip_Clock_SetSysClockDiv(1);
+
+	/* Setup FLASH access to 3 clocks */
+	Chip_FMC_SetFLASHAccess(FLASHTIM_50MHZ_CPU);
+
+	/* Set main clock source to the system PLL. This will drive 48MHz
+	   for the main clock and 48MHz for the system clock */
+	Chip_Clock_SetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
+}
+
 
 /*****************************************************************************
  * Public functions
@@ -59,11 +100,8 @@ const uint32_t ExtRateIn = 0;
 /* Set up and initialize hardware prior to call to main */
 void SystemInit(void)
 {
-#if defined(NO_BOARD_LIB)
-	/* Chip specific SystemInit */
-	Chip_SystemInit();
-#else
-	/* Board specific SystemInit */
-	Board_SystemInit();
-#endif
+	SystemSetupClocking();
+
+	// turn on GPIO clock
+	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_IOCON);
 }
