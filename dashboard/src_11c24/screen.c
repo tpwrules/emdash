805ff49ad16a5e78d67d17b17c8168e9bf85d127
Thomas Watson <twatson52@icloud.com>
2018-06-11 10:35:43 +0200

    remove pretty silly auto mode switch and always use auto mode

--- dashboard/src_11c24/screen.c
+++ dashboard/src_11c24/screen.c
@@ -5,8 +5,6 @@
 
 #include "t6963.h"
 
-#define USE_AUTO_MODE
-
 void scr_show_page(uint8_t text, uint8_t page) {
     if (text) {
         // set text home address
@@ -33,17 +31,12 @@ void scr_clear_page(uint8_t text, uint8_t page) {
     // set LCD address to clear region start
     lcd_send_acmd(0x24, addr);
 
-#ifdef USE_AUTO_MODE
     // start auto send mode
     lcd_send_0cmd(0xB0);
     for (int i=0; i<size; i++)
         lcd_send_auto(0, false);
     // end auto mode
     lcd_send_auto(0xB2, true);
-#else
-    for (int i=0; i<size; i++)
-        lcd_send_1cmd(0xC0, 0);
-#endif
 }
 
 void scr_draw_rect(uint32_t byte_addr, uint8_t w, uint8_t h, uint8_t color) {
@@ -56,30 +49,21 @@ void scr_draw_rect(uint32_t byte_addr, uint8_t w, uint8_t h, uint8_t color) {
         // position at the start of the line
         lcd_send_acmd(0x24, byte_addr);
         byte_addr += 64; // go to next row
-#ifdef USE_AUTO_MODE
         // start auto send mode
         lcd_send_0cmd(0xB0);
-#endif
+
         // loop pixels backwards
         for (int x=w; x>0; x-=6) {
-#ifdef USE_AUTO_MODE
             if (x < 6) // if less than a full byte's worth
                 // send end value
                 lcd_send_auto(end_color, false);
             else
                 // send main value
                 lcd_send_auto(main_color, false);
-#else
-            if (x < 6)
-                lcd_send_1cmd(0xC0, end_color);
-            else
-                lcd_send_1cmd(0xC0, main_color);
-#endif
         }
-#ifdef USE_AUTO_MODE
+
         // end auto mode
         lcd_send_auto(0xB2, true);
-#endif
     }
 }
 
@@ -100,10 +84,9 @@ void scr_draw_pic(uint32_t byte_addr, uint32_t pic_id, uint8_t inverted) {
         lcd_send_acmd(0x24, byte_addr);
         // increment to next row
         byte_addr += 64;
-#ifdef USE_AUTO_MODE
         // start auto send mode
         lcd_send_0cmd(0xB0);
-#endif
+
         for (int x=0; x<pic->width; x++) {
             out = 0xFF; // we need to output a byte
             do {
@@ -132,38 +115,25 @@ void scr_draw_pic(uint32_t byte_addr, uint32_t pic_id, uint8_t inverted) {
             if (inverted) out ^= 0xFF;
 
             // output the byte
-#ifdef USE_AUTO_MODE
             lcd_send_auto(out, false);
-#else
-            lcd_send_1cmd(0xC0, out);
-#endif
         }
+
         // end auto mode
-#ifdef USE_AUTO_MODE
         lcd_send_auto(0xB2, true);
-#endif
     }
 }
 
 void scr_draw_text(uint32_t text_addr, char *text) {
     // go to the requested address
     lcd_send_acmd(0x24, text_addr);
-#ifdef USE_AUTO_MODE
     // start auto mode
     lcd_send_0cmd(0xB0);
-#endif
     // and output text
     while (1) {
         char c = *text++;
         if (!c) break;
-#ifdef USE_AUTO_MODE
         lcd_send_auto(c, false);
-#else
-        lcd_send_1cmd(0xC0, c);
-#endif
     }
-#ifdef USE_AUTO_MODE
     // end auto mode
     lcd_send_auto(0xB2, true);
-#endif
 }
